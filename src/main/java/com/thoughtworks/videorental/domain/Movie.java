11cbffd70d672a1e0717a43291bb6d7b371d51c2
Sunit Parekh <parekh.sunit@gmail.com>
2011-04-21 19:09:13 +0530

    removed instances of Hibernate @Entity

--- src/main/java/com/thoughtworks/videorental/domain/Movie.java
+++ src/main/java/com/thoughtworks/videorental/domain/Movie.java
@@ -1,30 +1,27 @@
 package com.thoughtworks.videorental.domain;
 
-import javax.persistence.Entity;
-
-@Entity
 public class Movie {
-	public static final Price CHILDRENS = new ChildrensPrice();
-	public static final Price REGULAR = new RegularPrice();
-	public static final Price NEW_RELEASE = new NewReleasePrice();
+    public static final Price CHILDRENS = new ChildrensPrice();
+    public static final Price REGULAR = new RegularPrice();
+    public static final Price NEW_RELEASE = new NewReleasePrice();
 
-	private String title;
-	private Price price;
+    private String title;
+    private Price price;
 
-	public Movie(String title, Price price) {
-		this.title = title;
-		setPrice(price);
-	}
+    public Movie(String title, Price price) {
+        this.title = title;
+        setPrice(price);
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setPrice(Price price) {
-		this.price = price;
-	}
+    public void setPrice(Price price) {
+        this.price = price;
+    }
 
-	public Price getPrice() {
-		return price;
-	}
+    public Price getPrice() {
+        return price;
+    }
 }
