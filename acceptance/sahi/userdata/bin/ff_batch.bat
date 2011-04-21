d14765f2b9ec02a486599a70e741da5b915dcc57
Sunit Parekh <parekh.sunit@gmail.com>
2011-04-21 18:52:49 +0530

    removed Twist/selenium acceptance tests

--- acceptance/sahi/userdata/bin/ff_batch.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-SET SAHI_HOME=..\..
-SET SAHI_USERDATA_DIR=..
-java -cp %SAHI_HOME%\lib\ant-sahi.jar net.sf.sahi.test.TestRunner scripts/demo/demo.suite "C:\Program Files\Mozilla Firefox\firefox.exe" http://sahi.co.in/demo/ default localhost 9999 3 firefox.exe "-profile %SAHI_USERDATA_DIR%/browser/ff/profiles/sahi$threadNo -no-remote"
